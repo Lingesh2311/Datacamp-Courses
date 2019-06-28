14a38b89d0e36e6850c82e2f24463d0c3c878fc1
Lingesh K <lingesh.k.2311@gmail.com>
2019-06-29 00:23:17 +0100

    Commit Cluster.py

--- Unsupervised Learning/Chapter 1/Cluster1.py
+++ Unsupervised Learning/Chapter 1/Cluster1.py
@@ -2,10 +2,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 20 # Number of points in each cluster
-k = 3 # Number of clusters
-points = np.random.normal(loc=[0, 1, 2], scale=[0.5, 0.75, 1.0], size=(N, k))
-
-print(points)
-
-# Plotting the clusters
+# Plot 3 clusters
+for loc, scale, N, col, mark in zip([0, 2, 4], [1.0, 0.5, 0.25],[10, 30, 40], ['r', 'g', 'b'], ['*', '^', '.']):
+    print(N)
+    cluster = np.random.normal(loc=loc, scale=scale, size=(N, 1))
+    plt.scatter(np.arange(N), cluster, c=col, marker= mark)
+plt.show()
