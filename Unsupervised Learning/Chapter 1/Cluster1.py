37409b5e0829bb1bbe93816303b8f0df1c81fc4b
Lingesh K <lingesh.k.2311@gmail.com>
2019-06-29 00:49:32 +0100

    Commit Cluster1.py

--- Unsupervised Learning/Chapter 1/Cluster1.py
+++ Unsupervised Learning/Chapter 1/Cluster1.py
@@ -1,10 +1,30 @@
 # Importing packages
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.datasets.samples_generator import make_blobs
+
 
 # Plot 3 clusters
-for loc, scale, N, col, mark in zip([0, 2, 4], [1.0, 0.5, 0.25],[10, 30, 40], ['r', 'g', 'b'], ['*', '^', '.']):
-    print(N)
+plt.figure('3 varying clusters along vertical axis')
+for loc, scale, N, col, mark in zip([0, 2, 4], [1.0, 0.15, 0.125],[10, 10, 10], ['r', 'g', 'b'], ['*', '^', '.']):
     cluster = np.random.normal(loc=loc, scale=scale, size=(N, 1))
+    print(cluster.shape)
     plt.scatter(np.arange(N), cluster, c=col, marker= mark)
+plt.show()
+
+# Generate some data
+X, y_true = make_blobs(n_samples=400, centers=4,
+                       cluster_std=0.60, random_state=0)
+X = X[:, ::-1] # flip axes for better plotting
+
+# Create KMeans with 4z clusters
+model  = KMeans(n_clusters= 4, random_state= 0)
+
+# Fit the model to points
+labels = model.fit(X).predict(X)
+
+# Scatter plot
+plt.figure('Scatter plot - 4 clusters')
+plt.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap='viridis')
 plt.show()
