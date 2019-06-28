1a005d2dedc16602920258c07a677c6a71c3a61f
Lingesh K <lingesh.k.2311@gmail.com>
2019-06-29 00:49:55 +0100

    Add Plotting file

---
+++ Unsupervised Learning/Chapter 1/PlotCluster.py
@@ -0,0 +1,24 @@
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import cdist
+import matplotlib.pyplot as plt 
+from Cluster1 import X as X
+
+def plot_kmeans(kmeans, X, n_clusters=4, rseed=0, ax=None):
+    labels = kmeans.fit_predict(X)
+
+    plt.figure('Clusters with boundaries')
+    # plot the input data
+    ax = ax or plt.gca()
+    ax.axis('equal')
+    ax.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap='viridis', zorder=2)
+
+    # plot the representation of the KMeans model
+    centers = kmeans.cluster_centers_
+    radii = [cdist(X[labels == i], [center]).max()
+             for i, center in enumerate(centers)]
+    for c, r in zip(centers, radii):
+        ax.add_patch(plt.Circle(c, r, fc='#CCCCCC', lw=3, alpha=0.5, zorder=1))
+    plt.show()
+
+kmeans = KMeans(n_clusters= 4, random_state= 0)
+plot_kmeans(kmeans, X)
