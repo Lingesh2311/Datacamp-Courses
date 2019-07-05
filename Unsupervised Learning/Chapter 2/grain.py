fa60c54c767099c45836598f3f0fb6b8e0d0876f
Lingesh K <lingesh.k.2311@gmail.com>
2019-07-05 12:23:17 +0100

    Add linkage function example

---
+++ Unsupervised Learning/Chapter 2/grain.py
@@ -0,0 +1,17 @@
+# Use the linkage() funciton
+# Hierarchical clustering
+
+
+from scipy.cluster.hierarchy import linkage, dendrogram
+import matplotlib.pyplot as plt
+
+# Calculate the linkage: mergings
+mergings = linkage(samples, method='complete')
+
+# Plot the dendrogram, using varieties as labels
+dendrogram(mergings,
+           labels=varieties,
+           leaf_rotation=90,
+           leaf_font_size=6,
+)
+plt.show()
