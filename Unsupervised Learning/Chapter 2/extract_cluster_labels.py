14d5f127582a325b68cbf4e8339b79dc73a717df
Lingesh K <lingesh.k.2311@gmail.com>
2019-07-05 12:29:21 +0100

    Add extracting labels method

---
+++ Unsupervised Learning/Chapter 2/extract_cluster_labels.py
@@ -0,0 +1,20 @@
+# use the fcluster() function to extract the
+# cluster labels for this intermediate clustering,
+# and compare the labels with the grain varieties
+# using a cross-tabulation.
+
+# Perform the necessary imports
+import pandas as pd
+from scipy.cluster.hierarchy import fcluster
+
+# Use fcluster to extract labels: labels
+labels = fcluster(mergings, 6, criterion='distance')
+
+# Create a DataFrame with labels and varieties as columns: df
+df = pd.DataFrame({'labels': labels, 'varieties': varieties})
+
+# Create crosstab: ct
+ct = pd.crosstab(df['labels'], df['varieties'])
+
+# Display ct
+print(ct)
